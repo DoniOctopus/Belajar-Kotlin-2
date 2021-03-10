a8d7ec9dbe836e809aa2e3275ab8134d8fbb4a73
doni <donioctavian1016@gmail.com>
2021-03-10 11:02:39 +0700

    Belajar-Kotlon-2

---
+++ app/src/main/java/com/enigmacamp/belajarkotlin2/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.enigmacamp.belajarkotlin2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
